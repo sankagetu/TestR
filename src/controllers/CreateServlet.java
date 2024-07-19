2b4d702672d7bbdaa6deac109f974f5fe830119b
sankagetu <'masakiandosyh@gmail.com'>
2024-07-19 13:43:49 +0900

    コミット

--- src/controllers/CreateServlet.java
+++ src/controllers/CreateServlet.java
@@ -40,6 +40,9 @@ public class CreateServlet extends HttpServlet {
      * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
      */
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        // コメント
+
         String _token = request.getParameter("_token");
         if(_token != null && _token.equals(request.getSession().getId())) {
             EntityManager em = DBUtil.createEntityManager();
